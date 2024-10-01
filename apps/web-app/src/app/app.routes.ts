ba2d7c99b2d09275cd07c2f060fa826b46d3c57c
Andres Solano <andres.solano@gmail.com>
2024-10-01 17:10:51 -0600

    adding pokemon lazy loading component

--- apps/web-app/src/app/app.routes.ts
+++ apps/web-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'pokemon', pathMatch: 'full' },
+  {
+    path: 'pokemon',
+    loadComponent: () =>
+      import('./features/pokemon/pokemon.component').then(
+        (m) => m.PokemonComponent
+      ),
+  },
+];
